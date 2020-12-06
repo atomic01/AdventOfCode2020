f469d499a70b73284580b3a738d42b835964a5db
atomic01 <37533776+atomic01@users.noreply.github.com>
2020-12-06 22:22:46 +0000

    Add files via upload

---
+++ Day6/Part2/Source12.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int countVotingData(char *);
+
+int main()
+{
+	int i = 0, done = 0, endLineCounter = 0, result = 0;
+	char votingData[150] = {"\0"};
+	char buffer =  '\0' ;
+	FILE* fp = fopen("Puzzle.txt", "r+");
+
+	while (i < 1006) // to get past the text for the puzzle in the txt file where i stored the numbers
+	{
+		fscanf(fp, "%c", &buffer);
+		//printf("%c", buffer);
+		i++;
+	}
+
+	i = 0;
+	while (!done)
+	{
+		fscanf(fp, "%c", &buffer);
+		if(feof(fp))
+			done = 1;
+		else
+		{
+			if(buffer!='\n')
+				{
+					votingData[i] = buffer;
+					endLineCounter = 0;
+					i++;
+				}
+			else
+			{
+				if( endLineCounter == 0)
+				{
+					endLineCounter++;
+					votingData[i] = '#';
+					i++;
+				}
+				else if(endLineCounter == 1)
+				{
+					result += countVotingData(&votingData[0]);
+					memset(votingData, '\0', 150*sizeof(char));
+					i = 0;
+				}
+			}
+		}
+	}
+
+	if(feof(fp))
+	{
+		votingData[i] = '#';
+		result += countVotingData(&votingData[0]);
+	}
+
+	printf("%d", result);
+
+	return 0;
+}
+
+int countVotingData(char *votingData)
+{
+	int count = 0, numberOfVoters = 0, i = 0;
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int letterCounts[26] = {0};
+
+
+	while(votingData[i] != '\0')
+	{
+		if(votingData[i] == '#')
+			numberOfVoters++;
+		i++;
+	}
+
+	for(int j = 0; j < 26; j++)
+	{
+		i = 0;
+		while(votingData[i]!='\0')
+		{
+			if(votingData[i] == alphabet[j])
+				letterCounts[j]++;
+			i++;
+		}
+	}
+
+	for(int i = 0; i < 26; i++)
+		if(letterCounts[i] == numberOfVoters)
+			count++;
+
+	return count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
